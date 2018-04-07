fd108651c4bb915b81fe3f27eea4bb76a7724b2f
RafaMendes1101 <rafael.mendes1101@gmail.com>
2018-04-07 16:43:34 +0000

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -33,6 +33,14 @@ app.post('/user',(req,res) => {
    });
 });
 
+app.get('/todos', (req,res) =>{
+   Todo.find().then((todos) => {
+       res.send({todos});
+   }, (e) => {
+       res.status(400).send(e);
+   }) ;
+});
+
 app.listen(process.env.PORT, process.env.IP, () => {
     console.log('Deu bom')
 });
